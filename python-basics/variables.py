f1f47c1aac46b3cc2b05ad8dc644d58c0d9d3e2c
aru-ops <aruzantaboldieva@gmail.com>
2026-01-28 23:05:06 +0500

    Variable types

---
+++ python-basics/variables.py
@@ -0,0 +1,33 @@
+#example 1:
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#example 2:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#example 3:
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#example 4:
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+#example 5:
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()

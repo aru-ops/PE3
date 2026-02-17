7293aa23635bf5fdf0482f4f2c748b46812091e6
aru-ops <aruzantaboldieva@gmail.com>
2026-02-17 20:38:37 +0500

    example for classes

---
+++ Practice-03/classes/class_definition.py
@@ -0,0 +1,11 @@
+# Example 1: Basic class and object creation
+
+class Person:
+    """Simple class representing a person"""
+    
+    def greet(self):
+        print("Hello! I am a person.")
+
+# Создаем объект класса
+person1 = Person()
+person1.greet()

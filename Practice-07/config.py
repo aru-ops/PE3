5148759f77d42560cf91014941a61467f1354249
aru-ops <aruzantaboldieva@gmail.com>
2026-03-31 15:27:48 +0500

    commit for contacts

--- Practice-07/config.py
+++
@@ -1,8 +0,0 @@
-def config():
-    return {
-        "host": "localhost",
-        "database": "phonebook_db",
-        "user": "postgres",
-        "password": "12345678",
-        "port": 5432
-    }
